b0c28c4fb34540a460d0fbbde505040011b5d3d9
unknown <mohamad.romadhon@paramadina.ac.id>
2024-01-28 00:22:56 +0700

    update

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::latest()->paginate(5);
+        $products = Product::latest()->paginate(6);
         return response()->json([
             'success'   => true,
             'message'   => 'List Data Products',
